82b9d2e3c6c07aaaf52a9d77fead59dba537f4e8
Harrison Cole <harrison.d.cole@gmail.com>
2019-04-04 12:07:05 -0400

    added merge sort

--- mergesort.js
+++ mergesort.js
@@ -1,5 +1,39 @@
+/* eslint-disable complexity */
 const split = arr => {
   if (arr.length <= 1) return arr;
   const middle = Math.floor(arr.length / 2);
   return [arr.slice(0, middle), arr.slice(middle)];
 };
+
+const merge = (arr1, arr2) => {
+  const result = [];
+  while (arr1.length > 0 && arr2.length > 0) {
+    let first = arr1[0];
+    let second = arr2[0];
+      if (first <= second) {
+        result.push(first);
+        arr1.shift();
+      } else {
+        result.push(second);
+        arr2.shift();
+      }
+    }
+    arr1.length > 0 && result.push(...arr1);
+    arr2.length > 0 && result.push(...arr2);
+    arr1 = null;
+    arr2 = null;
+
+  return result;
+};
+
+const mergeSort = arr => {
+  if (arr.length <= 1) return arr;
+
+  const [left, right] =  split(arr);
+  if (left.length === 1 && right.length === 1) {
+   return merge(left, right);
+  } else {
+   return merge(mergeSort(left), mergeSort(right));
+  }
+
+}
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,13 +1,13 @@
-describe('merge sort', () => {
-  it('has a split function', () => {
+describe('split function', () => {
+  it('exists', () => {
     expect(typeof split).toEqual('function');
   });
 
-  it('returns the input if it\'s a one element array', () => {
+  it("returns the input if it's a one element array", () => {
     expect(split([1])).toEqual([1]);
-  })
+  });
 
-  it('split function splits arrays in two', () => {
+  it('splits arrays in two', () => {
     expect(split([1, 2])).toEqual([[1], [2]]);
   });
   it('handles arrays of odd lengths', () => {
@@ -15,4 +15,57 @@ describe('merge sort', () => {
   });
 });
 
+describe('merge function', () => {
+  it('returns an array', () => {
+    expect(merge([], [])).toEqual([]);
+  });
+  it('has an output array length equal to the lengths of its inputs', () => {
+    expect(merge([1], [2]).length).toEqual(2);
+  });
+  it('merges two sorted arrays into one', () => {
+    expect(merge([1], [2])).toEqual([1, 2]);
+    expect(merge([1, 2, 3, 4], [5, 6, 7, 8])).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+    expect(merge([0, 4, 5], [1, 9, 20])).toEqual([0, 1, 4, 5, 9, 20]);
+  });
+  it('handles arrays of different lengths', () => {
+    expect(merge([1, 9], [0, 2, 3, 10])).toEqual([0, 1, 2, 3, 9, 10]);
+    expect(merge([0, 2, 3, 10], [1, 9])).toEqual([0, 1, 2, 3, 9, 10]);
+  });
+});
 
+describe('merge sort function', () => {
+  it('returns an array', () => {
+    expect(Array.isArray(mergeSort([1, 3, 8, 2, 90, 15, 300]))).toEqual(true);
+  });
+  it('has an output with the same length as its input', () => {
+    expect(mergeSort([1, 3, 8, 2, 90, 15, 300]).length).toEqual(7);
+  });
+
+  it('works with an array of two elements', () => {
+    expect(mergeSort([2, 1])).toEqual([1, 2]);
+  });
+
+  it('works with an array of three elements', () => {
+    expect(mergeSort([3, 2, 1])).toEqual([1, 2, 3]);
+  })
+
+  it('returns the original array if the input is sorted', () => {
+    expect(mergeSort([])).toEqual([]);
+    expect(mergeSort([3, 3, 3, 3])).toEqual([3, 3, 3, 3]);
+    expect(mergeSort([1])).toEqual([1]);
+    expect(mergeSort([2, 3, 4, 5])).toEqual([2, 3, 4, 5]);
+  });
+  it('returns a sorted array', () => {
+    expect(mergeSort([1, 3, 8, 2, 90, 15, 300])).toEqual([
+      1,
+      2,
+      3,
+      8,
+      15,
+      90,
+      300,
+    ]);
+    expect(mergeSort([5, 4, 2, 1])).toEqual([1, 2, 4, 5]);
+    expect(mergeSort([1, 9, 0, 2, 3, 10])).toEqual([0, 1, 2, 3, 9, 10]);
+  });
+});
